417c4f43cba4d1dcad8dfee0a0598b9c6c73d208
Simon Duchastel <simon@duchastel.com>
2024-12-16 12:31:30 -0500

    WIP

--- src/day2.ts
+++ src/day2.ts
@@ -7,7 +7,7 @@ async function part1(useSampleData: Boolean = false): Promise<number> {
     var safeReports = 0;
     lines.map(line => {
         const report = line.split(/\s+/).map(Number);
-        if(isReportSafe(report)) {
+        if(isReportSafe(report, false)) {
             safeReports++;
         }
     });
@@ -17,25 +17,85 @@ async function part1(useSampleData: Boolean = false): Promise<number> {
 
 async function part2(useSampleData: Boolean = false): Promise<number> {
     const input = await readInputForDay(2, useSampleData);
-    return -1;
+    const lines = input.trim().split('\n');
+
+    var safeReports = 0;
+    lines.map(line => {
+        const report = line.split(/\s+/).map(Number);
+        if(isReportSafe(report, true)) {
+            safeReports++;
+        }
+    });
+
+    return safeReports;
 }
 
-function isReportSafe(report: number[]): boolean {
+function isReportSafe(
+    report: number[], 
+    useProblemDampener: Boolean, 
+    hasEncounteredIssue = false, // tracks whether we've encountered an issue the problem dampener can fix
+): boolean {
     // if the first is bigger than the second, the whole list should decrease
     var shouldDecrease = report[0] > report[1];
     for (var i = 1; i < report.length; i++) {
         if (shouldDecrease && report[i] >= report[i-1]) {
             // if the later one is bigger but we should decrease, it's unsafe
-            return false;
+            if (!useProblemDampener) {
+                return false;
+            }
+             
+            // problem dampener can only fix 1 issue
+            if (hasEncounteredIssue) {
+                return false;
+            }
+
+            // attempt to fix the issue
+            if (!hasEncounteredIssue) {
+                const removeAfter = report.filter((_, index) => index !== i);
+                const tryRemovingAfter = isReportSafe(removeAfter, useProblemDampener, true);
+                const removeBefore = report.filter((_, index) => index !== i - 1);
+                const tryRemovingBefore = isReportSafe(removeBefore, useProblemDampener, true);
+                return tryRemovingBefore || tryRemovingAfter;
+            } else {
+                return false;
+            }
         }
         if (!shouldDecrease && report[i-1] >= report[i]) {
-            // if the earlier one is bigger but we should increase, it's unsafe
-            return false;
+            // if the later one is bigger but we should decrease, it's unsafe
+            if (!useProblemDampener) {
+                return false;
+            }
+             
+            // problem dampener can only fix 1 issue
+            if (hasEncounteredIssue) {
+                return false;
+            }
+                
+            // attempt to fix the issue
+            const removeAfter = report.filter((_, index) => index !== i);
+            const tryRemovingAfter = isReportSafe(removeAfter, useProblemDampener, true);
+            const removeBefore = report.filter((_, index) => index !== i - 1);
+            const tryRemovingBefore = isReportSafe(removeBefore, useProblemDampener, true);
+            return tryRemovingBefore || tryRemovingAfter;
         }
 
         if (Math.abs(report[i] - report[i-1]) > 3) {
-            // if the increase/decrease is more than 3, it's unsafe
-            return false
+            // if the later one is bigger but we should decrease, it's unsafe
+            if (!useProblemDampener) {
+                return false;
+            }
+             
+            // problem dampener can only fix 1 issue
+            if (hasEncounteredIssue) {
+                return false;
+            }
+
+            // attempt to fix the issue
+            const removeAfter = report.filter((_, index) => index !== i);
+            const tryRemovingAfter = isReportSafe(removeAfter, useProblemDampener, true);
+            const removeBefore = report.filter((_, index) => index !== i - 1);
+            const tryRemovingBefore = isReportSafe(removeBefore, useProblemDampener, true);
+            return tryRemovingBefore || tryRemovingAfter;
         }
     }
 
@@ -47,6 +107,6 @@ console.log("Part 1");
 const partOneResult = await part1();
 console.log(partOneResult);
 
-// console.log("Part 2");
-// const partTwoResult = await part2();
-// console.log(partTwoResult);
+console.log("Part 2");
+const partTwoResult = await part2();
+console.log(partTwoResult);
