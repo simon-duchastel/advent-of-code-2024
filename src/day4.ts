75e5e116721041894ff15eea9db3d712ba35dabd
Simon Duchastel <simon@duchastel.com>
2024-12-21 13:26:58 -0500

    Part 2 WIP - has bug

--- src/day4.ts
+++ src/day4.ts
@@ -7,7 +7,7 @@ async function part1(useSampleData: Boolean = false): Promise<number> {
 
     var numXmas = 0;
     for (const x of allXs) {
-        numXmas += numXmasAtPosition(wordSearch, x);
+        numXmas += numWordsAtPosition(wordSearch, x, 'XMAS');
     }
 
     return numXmas;
@@ -15,8 +15,15 @@ async function part1(useSampleData: Boolean = false): Promise<number> {
 
 async function part2(useSampleData: Boolean = false): Promise<number> {
     const input = await readInputForDay(4, useSampleData);
-    
-    return -1;
+    const wordSearch = parseWordSearch(input);
+    const allMs = findAllChars(wordSearch, 'M');
+
+    var numMas = 0;
+    for (const m of allMs) {
+        numMas += numWordsAtPosition(wordSearch, m, 'MAS', true);
+    }
+
+    return numMas;
 }
 
 type WordSearch = string[][];
@@ -30,7 +37,7 @@ function findAllChars(wordSearch: WordSearch, char: string): Position[] {
     var positions: Position[] = []
     wordSearch.forEach((row, rowIndex) => {
         row.forEach((char, colIndex) => {
-            if (char === 'X') {
+            if (char === char) {
                 positions.push([rowIndex, colIndex]);
             }
         });
@@ -39,16 +46,25 @@ function findAllChars(wordSearch: WordSearch, char: string): Position[] {
     return positions;
 }
 
-// Returns the number of words 'XMAS' in any direction
+// Returns the number of words [word] in any direction
 // at the given position.
-function numXmasAtPosition(wordSearch: WordSearch, position: Position): number {
-    const word = 'XMAS';
-    var numXmas = 0;
-    const directions: [number, number][] = [
-        [-1, 0], [1, 0], [0, -1], [0, 1], // up, down, left, right
+// If [inX] is true, then this function returns 1
+// if the words appear in an 'X' formation and 0 otherwise.
+function numWordsAtPosition(
+    wordSearch: WordSearch, 
+    position: Position, 
+    word: string,
+    inX: boolean = false,
+): number {
+    var numWords = 0;
+    var directions: [number, number][] = [
         [-1, -1], [-1, 1], [1, -1], [1, 1] // up-left, up-right, down-left, down-right
     ];
-    
+    if (!inX) {
+        // if we're looking for words in an 'X' formation, we only care about
+        // the diaganols. Re-add the horizontal/verticals if not.
+        directions.push([-1, 0], [1, 0], [0, -1], [0, 1]) // up, down, left, right
+    }
 
     for (let [dx, dy] of directions) {
         let valid = true;
@@ -71,17 +87,27 @@ function numXmasAtPosition(wordSearch: WordSearch, position: Position): number {
 
         // If we haven't skipped yet, the entire word is found in this direction
         if (valid) {
-            numXmas++;
+            numWords++;
         }
     }
 
-    return numXmas;
+    if (inX) {
+        if (numWords >= 2) {
+            // if we match any 2 diaganols, it's in an 'X' formation
+            return 1;
+        } else {
+            return 0;
+        }
+    }
+    
+    // if we're not looking for 'X' formations, just return the words found
+    return numWords;
 }
 
 console.log("Part 1");
 const partOneResult = await part1();
 console.log(partOneResult);
 
-// console.log("Part 2");
-// const partTwoResult = await part2();
-// console.log(partTwoResult);
+console.log("Part 2");
+const partTwoResult = await part2(true);
+console.log(partTwoResult);
