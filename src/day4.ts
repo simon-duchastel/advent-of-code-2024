9f8479355e2cff851f0ba26257702ec73f965497
Simon Duchastel <simon@duchastel.com>
2024-12-21 13:05:07 -0500

    find all the X's

--- src/day4.ts
+++ src/day4.ts
@@ -3,6 +3,8 @@ import { readInputForDay } from './common/file.js'
 async function part1(useSampleData: Boolean = false): Promise<number> {
     const input = await readInputForDay(4, useSampleData);
     const wordSearch = parseWordSearch(input);
+    const allXs = findAllChars(wordSearch, 'X');
+    console.log(allXs);
 
     return -1;
 }
@@ -13,12 +15,25 @@ async function part2(useSampleData: Boolean = false): Promise<number> {
     return -1;
 }
 
-type WordSearch = string[][]
+type WordSearch = string[][];
+type Position = [number, number]; // row first, then column
 
 function parseWordSearch(input: string): WordSearch {
     return input.split('\n').map(line => line.split(''));
 }
 
+function findAllChars(wordSearch: WordSearch, char: string): Position[] {
+    var positions: Position[] = []
+    wordSearch.forEach((row, rowIndex) => {
+        row.forEach((char, colIndex) => {
+            if (char === 'X') {
+                positions.push([rowIndex, colIndex]);
+            }
+        });
+    });
+
+    return positions;
+}
 
 console.log("Part 1");
 const partOneResult = await part1(true);
