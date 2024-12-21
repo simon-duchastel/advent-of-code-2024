b71218bcd358a136141d794b13f7f76f15080f38
Simon Duchastel <simon@duchastel.com>
2024-12-21 12:51:00 -0500

    Part 2 WIP

--- src/day3.ts
+++ src/day3.ts
@@ -2,7 +2,7 @@ import { readInputForDay } from './common/file.js'
 
 async function part1(useSampleData: Boolean = false): Promise<number> {
     const input = await readInputForDay(3, useSampleData);
-    const mulInstructions = parseMulInstructions(input);
+    const mulInstructions = parseMulInstructions(input, false);
 
     var sum = 0;
     for (const instruction of mulInstructions) {
@@ -14,21 +14,43 @@ async function part1(useSampleData: Boolean = false): Promise<number> {
 
 async function part2(useSampleData: Boolean = false): Promise<number> {
     const input = await readInputForDay(3, useSampleData);
-    
-    return -1;
+    const mulInstructions = parseMulInstructions(input, true);
+
+    var sum = 0;
+    for (const instruction of mulInstructions) {
+        sum += instruction[0] * instruction[1];
+    }
+
+    return sum;
 }
 
 type MulInstruction = [number, number];
 
-function parseMulInstructions(input: string): MulInstruction[] {
-    const regex = /mul\([0-9]+,[0-9]+\)/g;
+function parseMulInstructions(input: string, withConditionals: boolean): MulInstruction[] {
+    const regex = /(mul\([0-9]+,[0-9]+\))|(do(n't)?\(\))/g;
     var mulInstructions: MulInstruction[] = [];
     const matches = input.match(regex);
     if (!matches || matches.length === 0) { // if we have no matches return empty list
         return [];
     }
 
+    var mulIsDisabled = false;
     for (const match of matches) {
+        if (match === "don't()") {
+            // mul instructions are now disabled
+            mulIsDisabled = true;
+            continue;
+        }
+        if (match === "do()") {
+            // mul instructions are now re-enabled
+            mulIsDisabled = false;
+            continue;
+        }
+        if (withConditionals && mulIsDisabled) {
+            // skip if mul instructions are disabled
+            continue;
+        }
+
         const mulRegex = /mul\(([0-9]+),([0-9]+)\)/;
         const mulMatches = match.match(mulRegex);
         if (!mulMatches || mulMatches.length < 3) {
@@ -49,6 +71,6 @@ console.log("Part 1");
 const partOneResult = await part1();
 console.log(partOneResult);
 
-// console.log("Part 2");
-// const partTwoResult = await part2(true);
-// console.log(partTwoResult);
+console.log("Part 2");
+const partTwoResult = await part2(true);
+console.log(partTwoResult);
