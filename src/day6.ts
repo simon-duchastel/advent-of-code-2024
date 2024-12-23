f9fd433a1bf4ce2617bcca26f3404a2e5550b9a9
Simon Duchastel <simon@duchastel.com>
2024-12-22 21:20:52 -0500

    boilerplate

---
+++ src/day6.ts
@@ -0,0 +1,21 @@
+import { readInputForDay } from './common/file.js';
+
+async function part1(useSampleData: Boolean = false): Promise<number> {
+    const input = await readInputForDay(6, useSampleData);
+
+    return -1;
+}
+
+async function part2(useSampleData: Boolean = false): Promise<number> {
+    const input = await readInputForDay(5, useSampleData);
+
+    return -1;
+}
+
+console.log("Part 1");
+const partOneResult = await part1(true);
+console.log(partOneResult);
+
+// console.log("Part 2");
+// const partTwoResult = await part2();
+// console.log(partTwoResult);
