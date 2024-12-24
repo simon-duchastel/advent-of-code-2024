643b556581656eaab775dbe8be00eeca3520e334
Simon Duchastel <simon@duchastel.com>
2024-12-23 21:13:37 -0500

    WIP

--- src/day7.ts
+++ src/day7.ts
@@ -3,8 +3,12 @@ import { readInputForDay } from './common/file.js';
 async function part1(useSampleData: Boolean = false): Promise<number> {
     const input = await readInputForDay(7, useSampleData);
     const instructions = parseInstructions(input);
+    const sum = instructions
+        .filter((instruction) => testOperators(instruction) !== undefined)
+        .flatMap((instruction) => instruction.numbers)
+        .reduce((sum, num) => sum + num, 0);
 
-    return -1;
+    return sum;
 }
 
 async function part2(useSampleData: Boolean = false): Promise<number> {
@@ -31,8 +35,58 @@ function parseInstructions(input: string): Instruction[] {
 }
 
 function testOperators(instruction: Instruction): Operator[] | undefined {
-    if (instruction.numbers.length == 0) {
-        if (instruction.expected)
+    // if there's one number left, check if we've reached the expected value
+    if (instruction.numbers.length === 1) {
+        if (instruction.expected !== instruction.numbers[0]) {
+            // if the last value doesn't match, the operators don't work
+            return undefined;
+        } else {
+            // if the last value matches then these operators work
+            return [];
+        }
+    }
+
+    // test all operations
+    var operators = attemptOperator('*', instruction);
+    if (operators) {
+        return operators;
+    }
+    operators = attemptOperator('/', instruction);
+    if (operators) {
+        return operators;
+    }
+    operators = attemptOperator('+', instruction);
+    if (operators) {
+        return operators;
+    }
+    operators = attemptOperator('-', instruction);
+    if (operators) {
+        return operators;
+    }
+}
+
+function attemptOperator(operator: Operator, instruction: Instruction): Operator[] | undefined {
+    var updatedInstruction = copyInstruction(instruction);
+
+    switch (operator) {
+        case '*': updatedInstruction.expected /= instruction.numbers[0]
+        case '/': updatedInstruction.expected *= instruction.numbers[0];
+        case '+': updatedInstruction.expected -= instruction.numbers[0]
+        case '-': updatedInstruction.expected += instruction.numbers[0];
+    }
+    updatedInstruction.numbers.splice(0);
+
+    var operatorsToReturn = testOperators(instruction);
+    if (operatorsToReturn) {
+        operatorsToReturn.push(operator);
+    }
+    return operatorsToReturn
+}
+
+function copyInstruction(instruction: Instruction): Instruction {
+    return {
+        expected: instruction.expected,
+        numbers: instruction.numbers.slice(),
     }
 }
 
