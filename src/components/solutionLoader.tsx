e49129646cfe05fe1e42657183f4a0a526b985a1
Simon Duchastel <simon@duchastel.com>
2024-12-26 09:50:48 -0500

    Add dynamic solution resolution

--- src/components/solutionLoader.tsx
+++ src/components/solutionLoader.tsx
@@ -1,95 +1,39 @@
 'use client';
 
-import { part1 as day1part1 } from "@/solutions/day1";
-import { part2 as day1part2 } from "@/solutions/day1";
-import { part1 as day2part1 } from "@/solutions/day2";
-import { part2 as day2part2 } from "@/solutions/day2";
-import { part1 as day3part1 } from "@/solutions/day3";
-import { part2 as day3part2 } from "@/solutions/day3";
-import { part1 as day4part1 } from "@/solutions/day4";
-import { part2 as day4part2 } from "@/solutions/day4";
-import { part1 as day5part1 } from "@/solutions/day5";
-import { part2 as day5part2 } from "@/solutions/day5";
-import { part1 as day6part1 } from "@/solutions/day6";
-import { part2 as day6part2 } from "@/solutions/day6";
-import { part1 as day7part1 } from "@/solutions/day7";
-import { part2 as day7part2 } from "@/solutions/day7";
 import { useState } from "react";
 
 type Props = {
   day: number,
   part: number,
   useSampleData?: boolean,
-}
+};
 
-export default function SolutionLoader({ 
-  day, 
-  part, 
+export default function SolutionLoader({
+  day,
+  part,
   useSampleData,
 }: Props) {
   const [data, setData] = useState("Click to load!");
-  let solutionFunction: (useSampleData: boolean) => Promise<number>;
-
-  // use JSON.stringify as a hack for equality check
-  switch (JSON.stringify([day, part])) {
-    case JSON.stringify([1, 1]): 
-      solutionFunction = day1part1;
-      break;
-    case JSON.stringify([1, 2]): 
-      solutionFunction = day1part2; 
-      break;
-    case JSON.stringify([2, 1]): 
-      solutionFunction = day2part1; 
-      break;
-    case JSON.stringify([2, 2]): 
-      solutionFunction = day2part2; 
-      break;
-    case JSON.stringify([3, 1]): 
-      solutionFunction = day3part1; 
-      break;
-    case JSON.stringify([3, 2]): 
-      solutionFunction = day3part2; 
-      break;
-    case JSON.stringify([4, 1]): 
-      solutionFunction = day4part1; 
-      break;
-    case JSON.stringify([4, 2]): 
-      solutionFunction = day4part2; 
-      break;
-    case JSON.stringify([5, 1]): 
-      solutionFunction = day5part1; 
-      break;
-    case JSON.stringify([5, 2]): 
-      solutionFunction = day5part2; 
-      break;
-    case JSON.stringify([6, 1]): 
-      solutionFunction = day6part1; 
-      break;
-    case JSON.stringify([6, 2]): 
-      solutionFunction = day6part2; 
-      break;
-    case JSON.stringify([7, 1]): 
-      solutionFunction = day7part1; 
-      break;
-    case JSON.stringify([7, 2]): 
-      solutionFunction = day7part2; 
-      break;
-    default: 
-      solutionFunction = () => Promise.reject(`Solution not found for day ${day} part ${part}`);
-  }
 
   const loadSolution = async () => {
-    setData("Loading...")
+    setData("Loading...");
     try {
+      const solutionPath = `@/solutions/day${day}`;
+      const solutionModule = await import(solutionPath);
+      const solutionFunction: (useSampleData: boolean) => Promise<number>
+        = solutionModule[`part${part}`];
+
+      if (typeof solutionFunction !== "function") {
+        throw new Error(`Solution not found for day ${day} part ${part}`);
+      }
+
       const result = await solutionFunction(useSampleData ?? false);
       setData(`${result}`);
-    } catch (error) {
-      console.error('Error loading solution:', error);
-      setData(`${error}`);
+    } catch (error: any) {
+      console.error("Error loading solution:", error);
+      setData(`Error: ${error.message}`);
     }
   };
 
-  return (
-    <button onClick={loadSolution}>{data}</button>
-  )
+  return <button onClick={loadSolution}>{data}</button>;
 }
