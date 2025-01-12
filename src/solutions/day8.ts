2f8abc37b599f72f825222ec40e1a47c0b8cb07f
Simon Duchastel <simon@duchastel.com>
2025-01-12 18:58:59 -0500

    Add end of file lines, add day8 command line code

--- src/solutions/day8.ts
+++ src/solutions/day8.ts
@@ -15,3 +15,13 @@ export async function part2(useSampleData: boolean = false): Promise<number> {
 
     return -1;
 }
+
+if (import.meta.url === `file://${process.argv[1]}`) {
+    console.log("Part 1");
+    const partOneResult = await part1();
+    console.log(partOneResult);
+
+    console.log("Part 2");
+    const partTwoResult = await part2();
+    console.log(partTwoResult);
+}
