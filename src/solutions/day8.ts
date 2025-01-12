f713617f90c682a8d9cc840ed5bc785954b9c2dc
Simon Duchastel <simon@duchastel.com>
2025-01-12 18:56:13 -0500

    day8 boilerplate

---
+++ src/solutions/day8.ts
@@ -0,0 +1,17 @@
+import { readInputForDay } from '@/common/file';
+
+export async function part1(useSampleData: boolean = false): Promise<number> {
+    const input = await readInputForDay(8, useSampleData);
+
+    // TODO
+
+    return -1;
+}
+
+export async function part2(useSampleData: boolean = false): Promise<number> {
+    const input = await readInputForDay(7, useSampleData);
+
+    // TODO
+
+    return -1;
+}
