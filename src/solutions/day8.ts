2c8557690686fd7e888d65dbbfc4843875624977
Simon Duchastel <simon@duchastel.com>
2025-01-15 23:02:38 -0500

    part 2 complete

--- src/solutions/day8.ts
+++ src/solutions/day8.ts
@@ -32,11 +32,52 @@ export async function part1(useSampleData: boolean = false): Promise<number> {
 }
 
 export async function part2(useSampleData: boolean = false): Promise<number> {
-    // const input = await readInputForDay(8, useSampleData);
+    const input = await readInputForDay(8, useSampleData);
 
-    // TODO
+    const antennas = parseAntennasFromInput(input);
+    const [maxX, maxY] = parseMapSizeFromInput(input);
 
-    return -1;
+    const antinodes: Coord[] = [];
+    antennas.forEach((antenna, index) => {
+        const [antennaType, antennaCoord] = antenna;
+        antennas.forEach((otherAntenna, otherIndex) => {
+            const [otherType, otherCoord] = otherAntenna;
+            if (otherIndex > index && antennaType === otherType) {
+                const [x1, y1] = antennaCoord;
+                const [x2, y2] = otherCoord;
+
+                const dx = (x1 - x2);
+                const dy = (y1 - y2);
+                
+                // add antinodes in one direction
+                let [newX, newY] = antennaCoord;
+                while (newX >= 0 && newY >= 0 && newX < maxX && newY < maxY) {
+                    antinodes.push([newX + dx, newY + dy]);
+                    newX += dx;
+                    newY += dy;
+                }
+
+                // add antinodes in the other direction
+                [newX, newY] = otherCoord;
+                while (newX >= 0 && newY >= 0 && newX < maxX && newY < maxY) {
+                    antinodes.push([newX - dx, newY - dy]);
+                    newX -= dx;
+                    newY -= dy;
+                }
+
+                // antennas count as antinodes too automatically
+                antinodes.push(antennaCoord);
+                antinodes.push(otherCoord);
+            }
+        });
+    });
+
+    const inBoundsAntinodes = antinodes.filter(([x, y]) =>
+         x >= 0 && y >= 0 && x < maxX && y < maxY
+    );
+    const uniqueAntinodes = filterUniqueCoords(inBoundsAntinodes);
+
+    return uniqueAntinodes.length;
 }
 
 type Coord = [number, number]
@@ -77,6 +118,8 @@ function filterUniqueCoords(coords: Coord[]): Coord[] {
     });
 }
 
+// prints a visual map of the antinodes. Useful for debugging.
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 function printMap(size: [number, number], antinodes: Coord[]) {
     const [maxX, maxY] = size;
     const map: string[][] = Array.from({ length: maxX }, () =>
