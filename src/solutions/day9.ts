43a173856b3626010024d2cf0db9f2fd7947b6fa
Simon Duchastel <simon@duchastel.com>
2025-01-19 15:20:09 -0500

    Part 2 WIP

--- src/solutions/day9.ts
+++ src/solutions/day9.ts
@@ -47,9 +47,44 @@ export async function part2(useSampleData: boolean = false): Promise<number> {
     const input = await readInputForDay(9, useSampleData);
     const spaces = parseInput(input);
 
-    // TODO - implement part 2
+    // increment through all the spaces and add them to a new rearranged list
+    const rearrangedSpaces: Space[] = [];
+    const remainingSpaces: Space[] = []; // the spaces which didn't fit
+    let leftIndex = 0;
+    let rightIndex = spaces.length - 1;
+    while (leftIndex <= rightIndex) {
+        if (spaces[leftIndex].blockType === "File") {
+            // we always add files
+            rearrangedSpaces.push(spaces[leftIndex]);
+            leftIndex++;
+            continue;
+        }
 
-    return calculateChecksum(spaces);
+        // add the right-hand blocks to the left
+        let spaceRemaining = spaces[leftIndex].blockSize;
+        while (spaceRemaining > 0 && rightIndex >= leftIndex) {
+            if (spaces[rightIndex].blockType === "File") {
+                if (spaceRemaining < spaces[rightIndex].blockSize) {
+                    remainingSpaces.unshift(spaces[rightIndex]);
+                    rightIndex--;
+                    continue; // if there's not enough space don't move it
+                }
+                rearrangedSpaces.push({
+                    blockId: spaces[rightIndex].blockId,
+                    blockType: "File",
+                    blockSize: spaces[rightIndex].blockSize,
+                });
+                spaceRemaining -= spaces[rightIndex].blockSize;
+            }
+            rightIndex--;
+        }        
+        leftIndex++;
+    }
+
+    // add all remaining blocks
+    rearrangedSpaces.push(...remainingSpaces);
+console.log(prettyPrintSpaces(rearrangedSpaces));
+    return calculateChecksum(rearrangedSpaces);
 }
 
 type SpaceType = "File" | "Free"
@@ -114,6 +149,6 @@ if (import.meta.url === `file://${process.argv[1]}`) {
     console.log(partOneResult);
 
     console.log("Part 2");
-    const partTwoResult = await part2();
+    const partTwoResult = await part2(true);
     console.log(partTwoResult);
 }
