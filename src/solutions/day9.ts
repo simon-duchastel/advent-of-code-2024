276eb0c4c7cc776a8885958bece7bcaf761d4786
Simon Duchastel <simon@duchastel.com>
2025-01-19 15:05:20 -0500

    sample working, fix lints

--- src/solutions/day9.ts
+++ src/solutions/day9.ts
@@ -1,15 +1,14 @@
 import { readInputForDay } from '@/common/file';
-import { BlockList } from 'net';
 
 export async function part1(useSampleData: boolean = false): Promise<number> {
     const input = await readInputForDay(9, useSampleData);
-    let spaces = parseInput(input);
+    const spaces = parseInput(input);
 
     // increment through all the spaces and add them to a new rearranged list
-    let rearrangedSpaces: Space[] = [];
+    const rearrangedSpaces: Space[] = [];
     let leftIndex = 0;
     let rightIndex = spaces.length - 1;
-    while (leftIndex < rightIndex) {
+    while (leftIndex <= rightIndex) {
         if (spaces[leftIndex].blockType === "File") {
             // we always add files
             rearrangedSpaces.push(spaces[leftIndex]);
@@ -20,21 +19,20 @@ export async function part1(useSampleData: boolean = false): Promise<number> {
             // search for the next file block from the right-hand side.
             rightIndex--;
         }
-        const rightHandSpace = spaces[rightIndex];
 
         // add the right-hand blocks to the left
         let spaceRemaining = spaces[leftIndex].blockSize;
-        while (spaceRemaining >= 0) {
-            let spaceToAdd = Math.min(spaceRemaining, rightHandSpace.blockSize);
+        while (spaceRemaining > 0) {
+            const spaceToAdd = Math.min(spaceRemaining, spaces[rightIndex].blockSize);
             const space: Space = {
-                blockId: rightHandSpace.blockId,
+                blockId: spaces[rightIndex].blockId,
                 blockType: "File",
                 blockSize: spaceToAdd,
             }
             rearrangedSpaces.push(space);
 
             spaceRemaining -= spaceToAdd;
-            if (spaceToAdd === rightHandSpace.blockSize) {
+            if (spaceToAdd === spaces[rightIndex].blockSize) {
                 rightIndex--;
             } else {
                 spaces[rightIndex].blockSize -= spaceToAdd;
@@ -43,12 +41,12 @@ export async function part1(useSampleData: boolean = false): Promise<number> {
         leftIndex++;
     }
 
-    return calculateChecksum(spaces);
+    return calculateChecksum(rearrangedSpaces);
 }
 
 export async function part2(useSampleData: boolean = false): Promise<number> {
     const input = await readInputForDay(9, useSampleData);
-    let spaces = parseInput(input);
+    const spaces = parseInput(input);
 
     // TODO - implement part 2
 
@@ -69,7 +67,7 @@ function parseInput(input: string): Space[] {
     return input.split("")
         .map((char) => parseInt(char)) 
         .map((int) => {
-            let space: Space = {
+            const space: Space = {
                 blockSize: int,
                 blockType: blockType,
                 blockId: blockType === "File" ? blockId : undefined,
@@ -82,6 +80,18 @@ function parseInput(input: string): Space[] {
             }
             return space
         });
+}
+
+// prints a visual map of the filesystem. Useful for debugging.
+// WARNING - can be long, so only use for small inputs
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+function prettyPrintSpaces(spaces: Space[]): string {
+    return spaces.map((space) => 
+        Array.from(
+            {length: space.blockSize}, 
+            () => space.blockType === "File" ? space.blockId : "."
+        ).join("")
+    ).join("");
 }
 
 function calculateChecksum(spaces: Space[]): number {
@@ -101,7 +111,7 @@ function calculateChecksum(spaces: Space[]): number {
 
 if (import.meta.url === `file://${process.argv[1]}`) {    
     console.log("Part 1");
-    const partOneResult = await part1();
+    const partOneResult = await part1(false);
     console.log(partOneResult);
 
     console.log("Part 2");
